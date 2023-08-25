ed6091ed29c96f22ea843f4ff87fd1eec70d5021
Promlert Lovichit <promlert@gmail.com>
2023-08-26 00:06:04 +0700

    Added REST data source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { StoreModule } from './store/store.module';
-import { RouterModule } from '@angular/router';
-import { CartDetailComponent } from './store/cart-detail.component';
-import { CheckoutComponent } from './store/checkout.component';
-import { StoreComponent } from './store/store.component';
+import { AppComponent } from "./app.component";
+import { StoreModule } from "./store/store.module";
+import { RouterModule } from "@angular/router";
+import { CartDetailComponent } from "./store/cart-detail.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { StoreComponent } from "./store/store.component";
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,13 +14,12 @@ import { StoreComponent } from './store/store.component';
     BrowserModule,
     StoreModule,
     RouterModule.forRoot([
-      { path: 'store', component: StoreComponent },
-      { path: 'cart', component: CartDetailComponent },
-      { path: 'checkout', component: CheckoutComponent },
-      { path: '**', redirectTo: '/store' },
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" },
     ]),
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
